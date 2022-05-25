5f13d948952f91ccccdbc6fc15348f8ce3b3421e
“Yelbaygit config --global user.email cnntylby@gmail.comgit config --global credential.username Yelbaygit config --global credential.password Cennet1983 <cnntylby@gmail.comgit config --global user.name Yelbay>
2022-05-24 22:15:16 -0400

    second commit

---
+++ src/test/java/com/myfirstproject/Day03_Assertions.java
@@ -0,0 +1,33 @@
+package com.myfirstproject;
+import org.junit.Assert;
+import org.junit.Test;
+public class Day03_Assertions {
+    @Test
+    public void assertions(){
+        /*
+         * *****ASSERTIONS*****
+         * if expected = actual
+         * if assertion fails we see red error
+         * if assertion pass we see green checkmark
+         *
+         * WHAT is difference between ASSERTION and VERIFICATION
+         * When ASSERTION fails ,test stops execution. ASSERTION means HARD ASSERT
+         * VERIFICATION means test continue to execute even after verification fails
+         * In TestNG is Soft Assert it means verification
+         * In verification,we we can mark more than one result as failed or passed
+         * */
+//       1.assertEquals("MESSAGE WHEN FAILS", "EXPECTED", "ACTUAL")
+//        Assert.assertEquals("TEST CASE IS FAILEd!!!","java","javascript");
+//        org.junit.ComparisonFailure: TEST CASE IS FAILEd!!!
+//        Expected :java
+//        Actual   :javascript
+        Assert.assertEquals(5,5);
+        //2. assertTrue(BOOLEAN)
+//        Assert.assertTrue("JAVA".contains("E"));
+//        java.lang.AssertionError
+        Assert.assertTrue("SELENIUM".contains("E"));//PASS
+        //3. assertFalse(BOOLEAN)
+        Assert.assertFalse("JAVA".contains("E"));//PASS
+        Assert.assertFalse("SELENIUM".contains("E"));//FAIL
+    }
+}
